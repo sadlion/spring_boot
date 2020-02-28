6be8c79f7c71defbd5c64e1935a721231d642ac4
Andrew Choi <sadlion@naver.com>
2020-02-28 21:47:58 +0900

    Add cucumber functions

--- src/main/java/io/cucumber/skeleton/Belly.java
+++ src/main/java/io/cucumber/skeleton/Belly.java
@@ -1,7 +1,18 @@
 package io.cucumber.skeleton;
 
 public class Belly {
+	public int total_cukes;
+	public int total_wait_time;
+	public int total_growl;
+	
     public void eat(int cukes) {
-
+    	total_cukes += cukes;
+    	
+    }
+    public void wait(int time) {
+    	total_wait_time += time;
+    }
+    public void growl() {
+    	total_growl += 1;
     }
 }
--- src/test/java/io/cucumber/skeleton/StepDefinitions.java
+++ src/test/java/io/cucumber/skeleton/StepDefinitions.java
@@ -1,11 +1,36 @@
 package io.cucumber.skeleton;
 
+//import com.hppk.cucumber.hello.Bag;
+
+import io.cucumber.skeleton.Belly;
+
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
 
 public class StepDefinitions {
+	private final Belly belly = new Belly();
+	
     @Given("I have {int} cukes in my belly")
     public void I_have_cukes_in_my_belly(int cukes) {
-        Belly belly = new Belly();
+        //Belly belly = new Belly();
         belly.eat(cukes);
     }
+    
+    @When("I wait {int} hour")
+    public void i_wait_hour(int int1) {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+    	//Belly belly = new Belly();
+        belly.wait(int1);
+    }
+
+    @Then("my belly should growl")
+    public void my_belly_should_growl() {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+    	//Belly belly = new Belly();
+        belly.growl();
+    }
+    
 }
